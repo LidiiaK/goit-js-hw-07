936b650521fc4c26cd4ac5545ef5b3339d750d87
LidiiaK <lida.kudriavtseva@gmail.com>
2022-08-23 23:18:02 +0300

    Done task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryCards = document.querySelector(".gallery");
+
+function createGallery(arrey) {
+    return arrey.reduce((acc, elem) => acc + `
+    <a class="gallery__item" href="${elem.original}">
+    <img
+    class="gallery__image"
+    src="${elem.preview}"
+    alt="${elem.description}"
+    />
+    </a>`, [])
+}
+const result = createGallery(galleryItems);
+galleryCards.insertAdjacentHTML("beforeend", result);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 console.log(galleryItems);
